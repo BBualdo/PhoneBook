ad4ee1ad47798327f9878711a4d84b4c186a7c19
Sebastian <mrseb10@gmail.com>
2024-04-18 11:37:19 +0200

    Contacts and Groups Menus

--- PhoneBook.BBualdo/AppEngine.cs
+++ PhoneBook.BBualdo/AppEngine.cs
@@ -28,12 +28,74 @@ internal class AppEngine
     switch (choice)
     {
       case "Manage Contacts":
+        ContactsMenu();
         break;
       case "Manage Groups":
+        GroupsMenu();
         break;
       case "Quit":
         AnsiConsole.Markup("[cyan1]Goodbye![/]\n");
         IsRunning = false;
+        break;
+    }
+  }
+
+  internal void ContactsMenu()
+  {
+    AnsiConsole.Clear();
+    ConsoleEngine.ShowTitle();
+
+    List<string> choices = [
+      "Back",
+      "Show Contacts",
+      "Create Contact",
+      "Update Contact",
+      "Delete Contact"
+      ];
+
+    string choice = ConsoleEngine.GetChoiceOption("", choices);
+
+    switch (choice)
+    {
+      case "Back":
+        return;
+      case "Show Contacts":
+        break;
+      case "Create Contact":
+        break;
+      case "Update Contact":
+        break;
+      case "Delete Contact":
+        break;
+    }
+  }
+
+  internal void GroupsMenu()
+  {
+    AnsiConsole.Clear();
+    ConsoleEngine.ShowTitle();
+
+    List<string> choices = [
+      "Back",
+      "Show Groups",
+      "Create Group",
+      "Update Group",
+      "Delete Group"
+      ];
+
+    string choice = ConsoleEngine.GetChoiceOption("", choices);
+
+    switch (choice)
+    {
+      case "Back":
+        return;
+      case "Show Groups":
+        break;
+      case "Create Group":
+        break;
+      case "Update Group":
+        break;
+      case "Delete Group":
         break;
     }
   }
